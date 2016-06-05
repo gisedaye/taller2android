d8d03249453a3f8c3f53e6799b9a1f17a0026968
fjfarina <74123698Fede>
2016-06-05 17:53:19 -0300

    Drawer and some unused classes removed

--- MatchApp/app/src/main/java/com/taller2/matchapp/MatchActivity.java
+++ MatchApp/app/src/main/java/com/taller2/matchapp/MatchActivity.java
@@ -2,9 +2,12 @@ package com.taller2.matchapp;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Window;
+import android.view.View;
 import com.andtinder.model.CardModel;
 import com.andtinder.view.CardContainer;
 import com.andtinder.view.SimpleCardStackAdapter;
@@ -15,7 +18,10 @@ public class MatchActivity extends ActionBarActivity {
     /**
      * This variable is the container that will host our cards
      */
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
     private CardContainer mCardContainer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,36 +30,38 @@ public class MatchActivity extends ActionBarActivity {
 
         mCardContainer = (CardContainer) findViewById(R.id.layoutview);
 
+          /* Assinging the toolbar object ot the view
+    and setting the the Action bar to our toolbar
+     */
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer);        // Drawer object Assigned to the view
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+
+        }; // Drawer Toggle Object Made
+        drawer.setDrawerListener(drawerToggle); // Drawer Listener set to the Drawer toggle
+        drawerToggle.syncState();               // Finally we set the drawer toggle sync State
+
+
         Resources r = getResources();
-
         SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
-
         adapter.setShouldFillCardBackground(true);
 
-        adapter.add(new CardModel("Title1", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title2", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title3", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title4", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title5", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title6", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title1", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title2", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title3", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title4", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title5", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title6", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title1", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title2", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title3", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title4", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title5", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title6", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title1", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title2", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title3", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
-        adapter.add(new CardModel("Title4", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
-        adapter.add(new CardModel("Title5", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
-        adapter.add(new CardModel("Title6", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
         adapter.add(new CardModel("Title1", "Person Name (26)", r.getDrawable(R.drawable.picture1)));
         adapter.add(new CardModel("Title2", "Person Name (26)", r.getDrawable(R.drawable.picture2)));
         adapter.add(new CardModel("Title3", "Person Name (26)", r.getDrawable(R.drawable.picture3)));
